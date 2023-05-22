b4f9ef41684d0b1fbcd4a3f9acaaa4087e515500
IKB <113422420+IKBxIK@users.noreply.github.com>
2023-05-22 21:52:44 +0500

    ikb.py

--- ikb.py
+++ ikb.py
@@ -1,2 +1,13 @@
-import ikb
-
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import ikb
+if bit == '32bit':
+    os.system('clear')
+     print('32 bit will be updated Soon')
